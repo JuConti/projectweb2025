f93e96800550b855c46d22603c8bfeece538b8fd
JuConti <juuuconti@gmail.com>
2025-11-05 18:50:28 +0100

    toggle theme

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,3 +50,14 @@ function updatePageMetadata(title, description) {
 		document.head.appendChild(metaDescription);
 	}
 }
+
+function toggleTheme() {
+  const html = document.documentElement;
+  const current = html.getAttribute('data-bs-theme');
+  const newTheme = current === 'dark' ? 'light' : 'dark';
+  
+  html.setAttribute('data-bs-theme', newTheme);
+  
+  // Save to localStorage
+  localStorage.setItem('theme', newTheme);
+}
